bb954994a125b7ef57dffe7e47107129a0cb8952
joshington <bbosalj@gmail.com>
2022-06-17 23:34:30 +0300

    environ

--- SparkRemitBackend/settings.py
+++ SparkRemitBackend/settings.py
@@ -28,7 +28,8 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY="n@$)g864tqkmzu0%pj6xz3(54&hx%9=dy_)87wui!iibj=d(0h"
+# SECRET_KEY="n@$)g864tqkmzu0%pj6xz3(54&hx%9=dy_)87wui!iibj=d(0h"
+SECRET_KEY=str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -204,8 +205,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT=587
-EMAIL_HOST_USER=os.environ['EMAIL_HOST_USER']
-EMAIL_HOST_PASSWORD=os.environ['EMAIL_HOST_PASSWORD']
+EMAIL_HOST_USER= str(os.getenv['EMAIL_HOST_USER'])
+EMAIL_HOST_PASSWORD=str(os.getenv['EMAIL_HOST_PASSWORD'])
 EMAIL_BACKEND ='django.core.mail.backends.smtp.EmailBackend'
 
 
