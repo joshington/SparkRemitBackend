4e5a4d2094b8055f41586d15303af773c0d3798c
joshington <bbosalj@gmail.com>
2022-06-17 23:37:43 +0300

    environ

--- SparkRemitBackend/settings.py
+++ SparkRemitBackend/settings.py
@@ -205,7 +205,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT=587
-EMAIL_HOST_USER= str(os.getenv['EMAIL_HOST_USER'])
+EMAIL_HOST_USER='bbosalj@gmail.com'
 EMAIL_HOST_PASSWORD=str(os.getenv['EMAIL_HOST_PASSWORD'])
 EMAIL_BACKEND ='django.core.mail.backends.smtp.EmailBackend'
 
