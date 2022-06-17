dfaecaa854a3ea072d3545e6b041d07e29649a86
joshington <bbosalj@gmail.com>
2022-06-17 23:26:47 +0300

    changes

--- SparkRemitBackend/settings.py
+++ SparkRemitBackend/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY=os.environ['SECRET_KEY']
+SECRET_KEY="n@$)g864tqkmzu0%pj6xz3(54&hx%9=dy_)87wui!iibj=d(0h"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
